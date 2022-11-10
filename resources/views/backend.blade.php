ebbc9d1a931202a60dbdcd4d81b97e7c8b6d7579
JoepJan van Hernen <joepjanvh@gmail.com>
2022-11-10 23:05:26 +0100

    layout update

--- resources/views/backend.blade.php
+++ resources/views/backend.blade.php
@@ -4,89 +4,58 @@
             {{ __('Backend') }}
         </h2>
     </x-slot>
-
+<!-- header part + username login confirm -->
     <div class="py-4 " >
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-2 text-xs bg-white dark:bg-gray-600 border-b border-gray-200 dark:border-gray-800">
+            <div class="bg-white dark:bg-gray-700 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-2 text-xs bg-white dark:bg-gray-700 border-b border-gray-200 dark:border-gray-800">
                     <b>{{ Auth::user()->name }}</b> is ingelogd in het Pooltochten Backend Systeem
                 </div>
             </div>
         </div>
     </div>
 
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <!-- div voor logo -->
 
-
-
-                <div class="mt-4 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                    <div class="grid grid-cols-1 md:grid-cols-2">
-                        <div class="p-6">
-                            <div class="flex items-center">
-                                <div class="ml-4 text-lg leading-7 font-semibold">Test</div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                
-                                </div>
-                            </div>
+<div class="grid grid-cols-9 gap-4">
+        <div class="col-span-2 mt-4 p-4 bg-white dark:bg-gray-700 overflow-hidden shadow sm:rounded-lg">
+            left column
+        </div>
+        
+        <div class="col-span-7 mt-4 p-4 bg-white dark:bg-gray-700 overflow-hidden shadow sm:rounded-lg">
+                <!-- container content here -->
+                <div class="grid grid-cols-9 gap-4">
+                    <div class="col-span-3 mx-auto "><img class="rounded-full h-28 w-28" src="site_img/profimg.jpg"></div>
+                        <div class="col-span-6 ">
+                            <div> <b>{{ Auth::user()->name }}</b></div>
+                            <div class="flex">
+                            <div class="mr-20"> <b>7</b> berichten</div>
+                            <div class="mr-20"> <b>7</b> volgers</div>
+                            <div class="mr-20"> <b>7</b> volgend</div>
                         </div>
 
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-                            <div class="flex items-center">
-                                
-                                <div class="ml-4 text-lg leading-7 font-semibold">Laracasts
-                                </div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700">
-                            <div class="flex items-center">
-                                
-                                <div class="ml-4 text-lg leading-7 font-semibold">Laravel News</div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                  
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-                            <div class="flex items-center">
-                                
-                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                   
-                                   </div>
-                            </div>
-                        </div>
                     </div>
+                    
+                    
                 </div>
 
-                <div class="sm:flex justify-center mt-4 sm:items-center sm:justify-between">
-                    <div class="text-center text-sm text-gray-500 sm:text-left">
-                        <div class="flex items-center">
-                           
+                <div class="border-t mt-8 border-gray-300"></div>
+                    <div class="mt-8 grid grid-cols-3 gap-3  ">
+                        <div class="p-2 col-span-1 mx-auto "><img class="hover:brightness-75 " src="site_img/profimg.jpg"></div>
+                        <div class="p-2 col-span-1 mx-auto "><img class="hover:brightness-75 " src="site_img/profimg.jpg"></div>
+                        <div class="p-2 col-span-1 mx-auto "><img class="hover:brightness-75 " src="site_img/profimg.jpg"></div>
+                        <div class="p-2 col-span-1 mx-auto "><img class="hover:brightness-75 " src="site_img/profimg.jpg"></div>
+                        <div class="p-2 col-span-1 mx-auto "><img class="hover:brightness-75 " src="site_img/profimg.jpg"></div>
+                        <div class="p-2 col-span-1 mx-auto "><img class="hover:brightness-75 " src="site_img/profimg.jpg"></div>
+                    </div>                
+        
+        </div>
+</div>        
 
-                        </div>
-                    </div>
-
-                    <div class="ml-4 text-center text-xs text-gray-500 sm:text-right sm:ml-0">
-                        Pooltochten 2022 &#169; 
-                    </div>
-                </div>
-            </div>
+        <div class="ml-4 mt-2 text-center text-xs text-gray-500 sm:text-right sm:ml-0">
+            Pooltochten 2022 &#169; 
+        </div>
+</div>
+            
 </x-app-layout>
